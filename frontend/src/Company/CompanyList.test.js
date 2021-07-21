a990d1312c6bb72eda6c2d0d26437666da2f1787
Alex Reitz <alex.j.reitz@gmail.com>
2021-07-21 19:10:05 -0400

    tests

--- frontend/src/Company/CompanyList.test.js
+++ frontend/src/Company/CompanyList.test.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "./App";
+import Companies from "./CompanyList";
 import { MemoryRouter } from "react-router";
 
 it("matches snapshot", function () {
   const { asFragment } = render(
     <MemoryRouter>
-      <App />
+      <Companies />
     </MemoryRouter>
   );
   expect(asFragment()).toMatchSnapshot();
